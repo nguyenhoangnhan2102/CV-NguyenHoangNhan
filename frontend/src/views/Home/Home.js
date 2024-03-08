cc570b6757af057f5e85c17b56c2ef93ab66669b
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-03-08 22:51:51 +0700

    08/03/2024

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Nav from '../Nav/Nav';
+import './Home.css';
 
 class Home extends React.Component {
     render() {
         return (
             <>
-                <div>
+                {/* <div>
                     <img src='../img/Kyouge (2).png' style={{ width: '500px', height: '500px' }} />
+                </div> */}
+                <div className='content'>
+                    <div className='a'>
+                        Hi
+                    </div>
+                    <div className='b'>
+                        <ul>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </div>
                 </div>
             </>
         )
--- frontend/src/views/Nav/Nav.js
+++ frontend/src/views/Nav/Nav.js
@@ -5,7 +5,7 @@ class Nav extends React.Component {
         return (
             <div className="container">
                 <div className="navbar">
-                    <ul>
+                    <ul className="nav-pc">
                         <li><a href="#home">TRANG CHỦ</a></li>
                         <li><a href="#news">THÔNG TIN</a></li>
                         <li><a href="#news">SỞ THÍCH</a></li>
@@ -17,6 +17,9 @@ class Nav extends React.Component {
                                 <a href="#3">Link 3</a>
                             </div>
                         </li>
+                        <div className="bar-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+                        </div>
                     </ul>
                 </div>
             </div>
