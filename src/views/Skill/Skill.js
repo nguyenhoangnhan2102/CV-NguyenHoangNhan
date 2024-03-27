d65d034e264b50cf71c6af4b6499d59cd38f9b67
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-03-27 10:25:36 +0700

    update

--- src/views/Skill/Skill.js
+++ src/views/Skill/Skill.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Skill.scss';
+import './SkillResponsive.scss';
 
 class Skill extends React.Component {
     render() {
@@ -35,34 +36,36 @@ class Skill extends React.Component {
                             <p className="name_skill">MySQL</p>
                         </div>
                     </div>
-                    <div className="my_project_container row">
-                        <div className="my_project_row col-12">
-                            <h1>My Project</h1>
+                    <div className="my_project_container">
+                        <div className="my_project_row">
+                            <h1>DỰ ÁN</h1>
                         </div>
                         {/* <div className="skill_row row"> */}
-                        <div className="my_project col-3">
-                            <img src="../img/What.jpg" />
-                            <h3>Xây dựng website bán điện thoại</h3>
-                            <p>Lorem ipsum dolor sit amet, consetetur
-                                sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat
-                                , sed diam voluptua.</p>
-                        </div>
-                        <div className="my_project col-3">
-                            <img src="../img/What.jpg" />
-                            <h3>Xây dựng website bán điện thoại</h3>
-                            <p>Lorem ipsum dolor sit amet, consetetur
-                                sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat
-                                , sed diam voluptua.</p>
-                        </div>
-                        <div className="my_project col-3">
-                            <img src="../img/What.jpg" />
-                            <h3>Xây dựng website bán điện thoại</h3>
-                            <p>Lorem ipsum dolor sit amet, consetetur
-                                sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat
-                                , sed diam voluptua.</p>
+                        <div className="my_project_deltail container">
+                            <div className="my_project">
+                                <img src="../img/What.jpg" />
+                                <h3>Xây dựng website bán điện thoại</h3>
+                                <p>Lorem ipsum dolor sit amet, consetetur
+                                    sadipscing elitr, sed diam nonumy eirmod tempor
+                                    invidunt ut labore et dolore magna aliquyam erat
+                                    , sed diam voluptua.</p>
+                            </div>
+                            <div className="my_project">
+                                <img src="../img/What.jpg" />
+                                <h3>Xây dựng website bán điện thoại</h3>
+                                <p>Lorem ipsum dolor sit amet, consetetur
+                                    sadipscing elitr, sed diam nonumy eirmod tempor
+                                    invidunt ut labore et dolore magna aliquyam erat
+                                    , sed diam voluptua.</p>
+                            </div>
+                            <div className="my_project">
+                                <img src="../img/What.jpg" />
+                                <h3>Xây dựng website bán điện thoại</h3>
+                                <p>Lorem ipsum dolor sit amet, consetetur
+                                    sadipscing elitr, sed diam nonumy eirmod tempor
+                                    invidunt ut labore et dolore magna aliquyam erat
+                                    , sed diam voluptua.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
